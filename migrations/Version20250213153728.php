fe8f2523aca11aa6647d82a380959c8ead8d59f2
jfplaper <jfplaper@gmail.com>
2025-02-13 17:25:07 +0100

    Añdido join

--- migrations/Version20250213153728.php
+++ migrations/Version20250213153728.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250212103631 extends AbstractMigration
+final class Version20250213153728 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,17 +21,17 @@ final class Version20250212103631 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE battle (id INT AUTO_INCREMENT NOT NULL, trainer_id INT NOT NULL, ally_pokemon_id INT NOT NULL, rival_pokemon_id INT NOT NULL, result INT DEFAULT NULL, INDEX IDX_13991734FB08EDF6 (trainer_id), INDEX IDX_1399173480BF0682 (ally_pokemon_id), INDEX IDX_139917344B8A08A8 (rival_pokemon_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE pokedex (id INT AUTO_INCREMENT NOT NULL, trainer_id INT NOT NULL, level INT DEFAULT NULL, strength INT DEFAULT NULL, UNIQUE INDEX UNIQ_6336F6A7FB08EDF6 (trainer_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE pokedex_pokemon (pokedex_id INT NOT NULL, pokemon_id INT NOT NULL, INDEX IDX_BD0379D5372A5D14 (pokedex_id), INDEX IDX_BD0379D52FE71C3E (pokemon_id), PRIMARY KEY(pokedex_id, pokemon_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE pokemon (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, type JSON NOT NULL, image VARCHAR(255) NOT NULL, level INT NOT NULL, strength INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_USERNAME (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE pokedex (id INT AUTO_INCREMENT NOT NULL, trainer_id INT NOT NULL, UNIQUE INDEX UNIQ_6336F6A7FB08EDF6 (trainer_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE pokedex_pokemon (id INT AUTO_INCREMENT NOT NULL, pokedex_id INT NOT NULL, pokemon_id INT NOT NULL, level INT NOT NULL, strength INT NOT NULL, INDEX IDX_BD0379D5372A5D14 (pokedex_id), INDEX IDX_BD0379D52FE71C3E (pokemon_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE pokemon (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, type JSON NOT NULL COMMENT \'(DC2Type:json)\', image VARCHAR(255) NOT NULL, level INT NOT NULL, strength INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_USERNAME (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE battle ADD CONSTRAINT FK_13991734FB08EDF6 FOREIGN KEY (trainer_id) REFERENCES `user` (id)');
         $this->addSql('ALTER TABLE battle ADD CONSTRAINT FK_1399173480BF0682 FOREIGN KEY (ally_pokemon_id) REFERENCES pokedex (id)');
         $this->addSql('ALTER TABLE battle ADD CONSTRAINT FK_139917344B8A08A8 FOREIGN KEY (rival_pokemon_id) REFERENCES pokemon (id)');
         $this->addSql('ALTER TABLE pokedex ADD CONSTRAINT FK_6336F6A7FB08EDF6 FOREIGN KEY (trainer_id) REFERENCES `user` (id)');
-        $this->addSql('ALTER TABLE pokedex_pokemon ADD CONSTRAINT FK_BD0379D5372A5D14 FOREIGN KEY (pokedex_id) REFERENCES pokedex (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE pokedex_pokemon ADD CONSTRAINT FK_BD0379D52FE71C3E FOREIGN KEY (pokemon_id) REFERENCES pokemon (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE pokedex_pokemon ADD CONSTRAINT FK_BD0379D5372A5D14 FOREIGN KEY (pokedex_id) REFERENCES pokedex (id)');
+        $this->addSql('ALTER TABLE pokedex_pokemon ADD CONSTRAINT FK_BD0379D52FE71C3E FOREIGN KEY (pokemon_id) REFERENCES pokemon (id)');
     }
 
     public function down(Schema $schema): void
